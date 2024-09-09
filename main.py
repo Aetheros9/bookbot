e070f2f475a3fa2a0fb77cf3fc378d73a883a820
Aetheros9 <akeer939@gmail.com>
2024-09-09 13:29:03 -0600

    add character count

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ def main():
     book_path = "github.com/Aetheros9/bookbot/books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
     print(f"{num_words} words found in the document")
-
+    print(chars_dict)
 
 def get_num_words(text):
     words = text.split()
@@ -18,5 +19,15 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 main()
